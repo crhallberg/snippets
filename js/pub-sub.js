fd429cb26423881497e1d9bc1cda7c59a690217f
Chris Hallberg <crhallberg@gmail.com>
2019-09-11 10:45:59 -0400

    Wrap pubsub for element choice.

--- js/pub-sub.js
+++ js/pub-sub.js
@@ -1,22 +1,35 @@
-function emit(name) {
-  var parameters = Array.from(arguments).slice(1); // capture additional arguments
-  if (parameters.length === 0) {
-    document.dispatchEvent(new Event(name));
-  } else {
-    var event = document.createEvent("CustomEvent");
-    // CustomEvent: name, canBubble, cancelable, detail
-    event.initCustomEvent(name, true, true, parameters);
-    document.dispatchEvent(event);
-  }
-}
+function pubsub(_el) {
+  const el = _el || document;
 
-// Listen shortcut to put everyone on the same element
-function listen(name, func) {
-  document.addEventListener(
-    name,
-    function listenPass(e) {
-      func.apply(null, e.detail);
-    },
-    false
-  );
+  function emit(name) {
+    var parameters = Array.from(arguments).slice(1); // capture additional arguments
+    if (parameters.length === 0) {
+      el.dispatchEvent(new Event(name));
+    } else {
+      var event = document.createEvent("CustomEvent");
+      // CustomEvent: name, canBubble, cancelable, detail
+      event.initCustomEvent(name, true, true, parameters);
+      el.dispatchEvent(event);
+    }
+    return el;
+  }
+
+  // Listen shortcut to put everyone on the same element
+  function listen(name, func) {
+    el.addEventListener(
+      name,
+      function listenPass(e) {
+        func.apply(null, e.detail);
+      },
+      false
+    );
+    return el;
+  }
+
+  function unlisten(name, func) {
+    el.removeEventListener(name, func);
+    return el;
+  }
+
+  return { emit, listen, unlisten };
 }
