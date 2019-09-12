6956119fd08f97abe8f52e1c84996ee6b5e1d653
Chris Hallberg <crhallberg@gmail.com>
2019-09-12 12:47:42 -0400

    Add debounce snippets.

---
+++ js/debounce.js
@@ -0,0 +1,47 @@
+// Debounce
+// Prevent a flood of calls
+// From: 1 2 3 -
+//   To: - - - 3
+function debounce(func, delay) {
+  let timeout;
+  return function() {
+    clearTimeout(timeout);
+    const context = this,
+      args = arguments;
+    timeout = setTimeout(function() {
+      func.apply(context, args);
+    }, delay);
+  };
+}
+
+// Throttle
+// Prevent calls for a certain amount of time
+// From: 1 2 3
+//   To: 1 - -
+function throttle(func, delay) {
+  let timeout = null;
+  return function() {
+    if (timeout === null) {
+      func.apply(this, arguments);
+      timeout = setTimeout(function() {
+        timeout = null;
+      }, delay);
+    }
+  };
+}
+
+// Accumulate
+// Save all calls within a certain time and debounce together
+// From: 1 2 3 -
+//   To: - - - [1,2,3]
+function accumulate(after, limit) {
+  let timeout, pool = [];
+  return function(op) {
+    clearTimeout(timeout);
+    pool.push(op);
+    timeout = setTimeout(function() {
+      after(pool);
+      pool = [];
+    }, limit);
+  };
+}
